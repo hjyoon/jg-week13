e897ae5706042ec25990f799835fef58904c2bd7
hjyoon <hjyoon@kakao.com>
2024-11-02 23:52:34 +0900

    Impl comment CRUD

--- src/app.js
+++ src/app.js
@@ -141,6 +141,83 @@ app.delete("/api/posts/:post_id", async (req, res, next) => {
   }
 });
 
+app.get("/api/posts/:post_id/comments", async (req, res, next) => {
+  const { post_id } = req.params;
+  try {
+    const db = getDb();
+    const comments = await db
+      .collection("comments")
+      .find({ post_id })
+      .sort({ created_at: -1 })
+      .toArray();
+    res.status(200).json(buildResponse(CODE_1, { data: comments }));
+  } catch (e) {
+    next();
+  }
+});
+
+app.post("/api/posts/:post_id/comments", async (req, res, next) => {
+  const { post_id } = req.params;
+  const { content } = req.body;
+  if (!content) {
+    return res.status(400).json(CODE_3);
+  }
+
+  try {
+    const db = getDb();
+    await db
+      .collection("comments")
+      .insertOne({ post_id, content, created_at: new Date() });
+    res.status(201).json(CODE_1);
+  } catch (e) {
+    next();
+  }
+});
+
+app.put("/api/posts/:post_id/comments/:comment_id", async (req, res, next) => {
+  const { post_id, comment_id } = req.params;
+  const { content } = req.body;
+  if (!content) {
+    return res.status(400).json(CODE_3);
+  }
+
+  try {
+    const db = getDb();
+    const comment = await db
+      .collection("comments")
+      .updateOne(
+        { _id: new ObjectId(comment_id), post_id },
+        { $set: { content } }
+      );
+    if (!comment) {
+      return resNotFound(res);
+    }
+    res.status(200).json(CODE_1);
+  } catch (e) {
+    next();
+  }
+});
+
+app.delete(
+  "/api/posts/:post_id/comments/:comment_id",
+  async (req, res, next) => {
+    const { post_id, comment_id } = req.params;
+
+    try {
+      const db = getDb();
+      const comment = await db
+        .collection("comments")
+        .deleteOne({ _id: new ObjectId(comment_id), post_id });
+      if (!comment) {
+        return resNotFound(res);
+      }
+      noContent(res);
+    } catch (e) {
+      next();
+    }
+  }
+);
+
 app.use((req, res, next) => {
   resNotFound(res);
 });
