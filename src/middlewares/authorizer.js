7a3539f5617ef1773d3a267cf9538aadcd5f1eae
hjyoon <hjyoon@kakao.com>
2024-11-03 20:09:12 +0900

    Modify checkToken to allow processing of token from cookie

--- src/middlewares/authorizer.js
+++ src/middlewares/authorizer.js
@@ -3,7 +3,8 @@ import { CODE_4 } from "../config/detailCode.js";
 import { JWT_SECRET } from "../config/const.js";
 
 export function checkToken(req, res, next) {
-  const token = req.headers?.authorization?.split("Bearer ")[1];
+  const token =
+    req.headers?.authorization?.split("Bearer ")[1] || req.cookies?.accessToken;
 
   try {
     const decodedToken = jwt.verify(token, JWT_SECRET);
